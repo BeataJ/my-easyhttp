ab8fa084e5baf83f4b2b83df1bef4cea270c663f
beata <beata@emc2wave.com>
2019-11-06 16:52:17 -0800

    sumulation of errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = new easyHTTP;
 
 // GET Posts
-const posts = http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
+http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
   if(err) {
     console.log(err)
   } else {
