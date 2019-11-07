d924e1f5b1c7d27ca77352bce4fe06dd5f539165
beata <beata@emc2wave.com>
2019-11-06 16:48:32 -0800

    add if  else statment for errors

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const http = new easyHTTP;
 
 // GET Posts
 const posts = http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
-  console.log(posts);
+  if(err) {
+    console.log(err)
+  } else {
+    console.log(posts);
+  }
+  
 });
 
