756cec6e38f4104885b5c0d46ef6ba13217e11eb
beata <beata@emc2wave.com>
2019-11-06 17:28:35 -0800

    add put request

--- app.js
+++ app.js
@@ -27,7 +27,18 @@ const data = {
 };
 
 // Create Post request
-http.post("https://jsonplaceholder.typicode.com/posts", data, function(
+// http.post("https://jsonplaceholder.typicode.com/posts", data, function(
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+http.put("https://jsonplaceholder.typicode.com/posts/1", data, function(
   err,
   post
 ) {
