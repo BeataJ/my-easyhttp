b62ef8086d75a6e855cee3d9e9aa51a6a72ea82b
beata <beata@emc2wave.com>
2019-11-06 17:42:46 -0800

    add delete response

--- app.js
+++ app.js
@@ -38,13 +38,23 @@ const data = {
 //   }
 // });
 
-http.put("https://jsonplaceholder.typicode.com/posts/1", data, function(
-  err,
-  post
-) {
-  if (err) {
-    console.log(err);
+// http.put("https://jsonplaceholder.typicode.com/posts/1", data, function(
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', (err, response) => {
+  if(err) {
+    console.log(err)
   } else {
-    console.log(post);
+    console.log(response);
   }
+
 });
