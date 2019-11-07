669ef3b20322e612ebe9ae38fc088cf26d0e386b
beata <beata@emc2wave.com>
2019-11-06 17:21:00 -0800

    create data and post request

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = new easyHTTP;
+const http = new easyHTTP();
 
 // // GET Posts
 // http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
@@ -7,16 +7,33 @@ const http = new easyHTTP;
 //   } else {
 //     console.log(posts);
 //   }
-  
+
 // });
 
 // GET Single Post
-http.get('https://jsonplaceholder.typicode.com/posts/2', (err, post) => {
-  if(err) {
-    console.log(err)
+// http.get('https://jsonplaceholder.typicode.com/posts/2', (err, post) => {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(post);
+//   }
+
+// });
+
+// Create data
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post"
+};
+
+// Create Post request
+http.post("https://jsonplaceholder.typicode.com/posts", data, function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err);
   } else {
     console.log(post);
   }
-  
 });
-
