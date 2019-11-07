02bf60bbcb9c91635d48dd9a74464e69388e1b80
beata <beata@emc2wave.com>
2019-11-06 17:09:20 -0800

    add request for single post

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const http = new easyHTTP;
 
-// GET Posts
-http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
+// // GET Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(posts);
+//   }
+  
+// });
+
+// GET Single Post
+http.get('https://jsonplaceholder.typicode.com/posts/1', (err, post) => {
   if(err) {
     console.log(err)
   } else {
-    console.log(posts);
+    console.log(post);
   }
   
 });
