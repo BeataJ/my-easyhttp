8b719f75cd5bce1a9d0fdccdea464f30094311f5
beata <beata@emc2wave.com>
2019-11-06 16:42:21 -0800

    add callback get

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const http = new easyHTTP;
 
 // GET Posts
-http.get('https://jsonplaceholder.typicode.com/posts');
+const posts = http.get('https://jsonplaceholder.typicode.com/posts', (posts) => {
+  console.log(posts);
+});
+
